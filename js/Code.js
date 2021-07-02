40cd98da20e9d2a41fffd3786c591de058610edd
nagasai488 <79254893+nagasai488@users.noreply.github.com>
2021-07-02 22:17:44 +0530

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,39 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+const accessCode4 = "BACKGROUND";
+const accessCode5 = "TREASURE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always Changing, Not Constant !!", 100,70)
+// add code for displaying rest of the names and the hints.
+fill("white")
+textSize(15)
+text("N F T C U O N I", 700,50)
+fill("lightblue")
+text("Hint: Programme !!", 700,70)
+
+    fill("white")
+    textSize(15)
+    text("E D T A S A B A", 100,310)
+    fill("lightblue")
+    text("Hint: Collection Of Data !!", 100,330)
+
+    fill("white")
+    textSize(15)  
+    text("G O K U A B N G C R", 700,310)
+    fill("lightblue")
+    text("Hint: Behind Something  !!", 700,330)
+    
+    fill("white")
+    textSize(15)  
+    text("U E A S R T R E", 400,180) 
+    fill("lightblue")
+    text("Hint: Hidden Wealth !!", 400,200)
+  
+}
